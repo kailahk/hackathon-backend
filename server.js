22c88eed266e3b1f57de01640954c14740efdce8
unknown <maryrodri3210@gmail.com>
2023-07-10 18:25:58 -0500

    added require dotenv

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ const cors = require("cors")
 
 const app = express();
 app.use(cors())
+
+require("dotenv").config();
+//require("./config/db.connection");
+
+//saveuser
+//getuser
+//getcalender
+//savecalender
+//getnote
+//savenote
+//saveopneuiresponse
+//getopenuiresponse
+app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
