777df7f7df1447ec5aad338d836bd1f0a815f31a
Mary Rodriguez <maryrodri3210@gmail.com>
2023-07-12 12:09:28 -0500

    add to server.js points to index.html

--- server.js
+++ server.js
@@ -21,6 +21,18 @@ app.use("/info", infoController)
 app.get("/", (req, res) => {
     res.send("Hello World");
   });
+  const path = require('path');
+  const favicon = require('serve-favicon');
+  
+  
+  app.use(logger('dev'));
 
+  
+  app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+  app.use(express.static(path.join(__dirname, 'build')));
+  
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
