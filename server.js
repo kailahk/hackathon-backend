bf7829e81c6b32cf2a52693de3845dc2412fc1d7
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-07-12 13:20:51 -0700

    Connects router to app; Comments out code that was causing errors until they are needed/removed.

--- server.js
+++ server.js
@@ -3,29 +3,39 @@ const cors = require("cors")
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
-const passport = require('passport');
+// const passport = require('passport');
+// const passport = require('./config/passport');
+const { handleErrors } = require('./middleware/error-handlers');
 
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+// Converts json strings to an object for the request.
+app.use(express.json());
 
-require("dotenv").config();
-require("./config/db.connection");
+require('dotenv').config();
+require('./config/db.connection');
 require('./config/passport');
 
-app.use(cookieParser());
+// app.use(cookieParser());
 
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
+// app.use(
+// 	session({
+// 		secret: process.env.SECRET,
+// 		resave: false,
+// 		saveUninitialized: true,
+// 	})
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// app.use(passport.initialize());
+// app.use(passport.session());
+app.get('/', (req, res) => {
+	res.send('Hello World');
 });
 
+app.use('/users', require('./routes/user'));
+
+app.use(handleErrors);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
