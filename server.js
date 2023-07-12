a0197bf9716921310dd024747565d29837945830
kailahk <kailahkorsh@me.com>
2023-07-12 10:22:23 -0700

    unused changes

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require("express")
+const path = require('path');
+const favicon = require('serve-favicon');
+var logger = require('morgan');
 const cors = require("cors")
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var session = require('express-session');
 const passport = require('passport');
-
-const app = express();
-app.use(cors())
-
 require("dotenv").config();
 require("./config/db.connection");
 require('./config/passport');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// app.use(require('./config/checkToken'))
+
+app.use(cors())
+
 
 app.use(cookieParser());
 
@@ -24,6 +35,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX/API requests
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
