269da5e8ed3c6f3b1f0a2efa912ac9157207a75d
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-07-12 13:09:35 -0700

    Changes error messages.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,48 +1,21 @@
-const passport = require('passport');
+const passport = require('../config/passport');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { Strategy, ExtractJwt } = require('passport-jwt');
-const User = require('../models/user');
 const jwtDecode = require('jwt-decode');
 
-// NODE_ENV is a Heroku variable.
-// This prevents dotenv throwing errors when deployed.
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
-}
+require('dotenv').config();
 
 const secret = process.env.JWT_SECRET;
 
-/* ===== Passport Config ===== */
-
-const strategyOptions = {
-	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-	secretOrKey: secret,
-};
-
-const strategy = new Strategy(strategyOptions, (jwt_payload, done) => {
-	User.findById(jwt_payload.id)
-		.then((user) => done(null, user))
-		.catch((error) => done(error));
-});
-
-// Register the strategy with passport to enable passport.authenticate() method.
-passport.use(strategy);
-passport.initialize();
-
-/* ===== Token Config ===== */
-
-// Authenticate method to use with routes.
 const requireToken = passport.authenticate('jwt', { session: false });
 
 function createUserToken(request, user) {
-	// Validate user login.
 	if (
 		!user ||
 		!request.body.password ||
 		!bcrypt.compareSync(request.body.password, user.password)
 	) {
-		const error = new Error('The provided username or password is incorrect');
+		const error = new Error('Failed to create user token');
 		error.statusCode = 422;
 		throw error;
 	}
@@ -55,7 +28,7 @@ function getIdFromToken(request) {
 	const token = request.headers.authorization.slice(7);
 
 	if (!token) {
-		const error = new Error('The credentials provided is incorrect');
+		const error = new Error('Failed to get ID from token');
 		error.statusCode = 422;
 		throw error;
 	}
